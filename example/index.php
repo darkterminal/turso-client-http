e1f30e3bb5f0a30e8143ef56bfa3d6fa8ee6bdea
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-22 00:09:11 +0700

    chore(example): adding more example

--- example/index.php
+++ example/index.php
@@ -1,19 +1,37 @@
 <?php
 
+use Darkterminal\DataType;
+use Darkterminal\SadQuery;
 use Darkterminal\TursoHTTP;
 
-require_once '../src/TursoHTTP.php';
+require_once getcwd() . '/src/SadQuery.php';
+require_once getcwd() . '/src/TursoHTTP.php';
 
 $databaseName       = "database-name";
 $organizationName   = "organization-name";
 $token              = "your-turso-database-token";
 
-$tursoAPI = new TursoHTTP($dabaseBane, $organizationName, $token);
+$tursoAPI = new TursoHTTP($databaseName, $organizationName, $token);
+$query = new SadQuery();
 
-$queryResponse = $tursoAPI
-    ->addRequest("execute", "CREATE TABLE users (userID TEXT NOT NULL PRIMARY KEY, firstName TEXT NOT NULL, lastName TEXT NOT NULL, email TEXT NOT NULL UNIQUE)")
+$createTableUsers = $query->createTable('users')
+    ->addColumn('userId', DataType::INTEGER, ['PRIMARY KEY', 'AUTOINCREMENT'])
+    ->addColumn('name', DataType::TEXT)
+    ->addColumn('email', DataType::TEXT)
+    ->endCreateTable();
+
+$createNewUser = $query->insert('users', ['name' => 'darkterminal', 'email' => 'darkterminal@duck.com'])->getQuery();
+
+$tableCreated = $tursoAPI
+    ->addRequest("execute", $createTableUsers)
     ->addRequest("close")
     ->queryDatabase()
     ->toJSON();
+echo $tableCreated . PHP_EOL;
 
-echo $queryResponse;
+$userCreated = $tursoAPI
+    ->addRequest("execute", $createNewUser)
+    ->addRequest("close")
+    ->queryDatabase()
+    ->toJSON();
+echo $userCreated . PHP_EOL;
