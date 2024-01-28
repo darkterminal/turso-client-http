f4dae6aabeeb1da4c55d4f96243e793a7870327d
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-28 14:30:18 +0700

    feat(core): add locations endpoint

---
+++ src/core/Repositories/locations.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'list' => [
+        'method'    => 'GET',
+        'url'       => $baseURL . '/locations'
+    ],
+    'closest_region' => [
+        'method'    => 'GET',
+        'url'       => 'https://region.turso.io'
+    ]
+];
