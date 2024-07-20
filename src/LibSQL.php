57749023669505e536efa157e9dd3de689e31af2
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-07-21 04:46:17 +0700

    feat(core): add batch query execution and improve transaction handling

--- src/LibSQL.php
+++ src/LibSQL.php
@@ -2,13 +2,21 @@
 
 namespace Darkterminal\TursoHttp;
 
+use Exception;
+use Darkterminal\TursoHttp\core\Utils;
+use Darkterminal\TursoHttp\core\Request;
+use Darkterminal\TursoHttp\core\LibSQLError;
 use Darkterminal\TursoHttp\core\Http\LibSQLResult;
 use Darkterminal\TursoHttp\core\Http\LibSQLStatement;
 use Darkterminal\TursoHttp\core\Http\LibSQLTransaction;
-use Darkterminal\TursoHttp\core\LibSQLError;
-use Darkterminal\TursoHttp\core\Request;
-use Darkterminal\TursoHttp\core\Utils;
 
+/**
+ * Class LibSQL
+ *
+ * A class to interact with the LibSQL database over HTTP.
+ *
+ * @package Darkterminal\TursoHttp
+ */
 class LibSQL
 {
     /**
@@ -17,17 +25,17 @@ class LibSQL
     public const int LIBSQL_ASSOC = 1;
 
     /**
-     * Return numerical array
+     * Return numerical array.
      */
     public const int LIBSQL_NUM = 2;
 
     /**
-     * Return both associative and numerical array
+     * Return both associative and numerical array.
      */
     public const int LIBSQL_BOTH = 3;
 
     /**
-     * Return a result sets
+     * Return all result sets.
      */
     public const int LIBSQL_ALL = 4;
 
@@ -36,24 +44,34 @@ class LibSQL
      *
      * @var string
      */
-    protected $baseURL;
+    protected string $baseURL;
 
     /**
      * Turso Database Auth Token
      *
      * @var string
      */
-    protected $authToken;
+    protected string $authToken;
 
+    /**
+     * @var Request The HTTP request handler.
+     */
     protected Request $http;
 
+    /**
+     * @var int The number of affected rows.
+     */
     protected int $affected_rows = 0;
+
+    /**
+     * @var bool Whether auto commit is enabled.
+     */
     public bool $auto_commit = true;
 
     /**
      * LibSQL HTTP Instance
      *
-     * @param string $dsn your dsn config
+     * @param string $dsn Your DSN config.
      */
     public function __construct(string $dsn)
     {
@@ -63,27 +81,54 @@ class LibSQL
         $this->http = new Request($this->baseURL, $this->authToken);
     }
 
+    /**
+     * Get the base URL of the database.
+     *
+     * @return string The base URL.
+     */
     public function getBaseUrl(): string
     {
         return $this->baseURL;
     }
 
+    /**
+     * Get the authentication token.
+     *
+     * @return string The authentication token.
+     */
     public function getAuthToken(): string
     {
         return $this->authToken;
     }
 
+    /**
+     * Get the HTTP request handler.
+     *
+     * @return Request The request handler.
+     */
     public function getConnection(): Request
     {
         return $this->http;
     }
 
+    /**
+     * Get the database version.
+     *
+     * @return string The database version.
+     */
     public function version(): string
     {
         $response = Utils::makeRequest('GET', "{$this->baseURL}/version", $this->authToken);
         return $response;
     }
 
+    /**
+     * Execute a query and return the results.
+     *
+     * @param string $sql The SQL query.
+     * @param array $parameters The query parameters.
+     * @return LibSQLResult The query results.
+     */
     public function query(string $sql, array $parameters = []): LibSQLResult
     {
         $results = $this->http->prepareRequest($sql, $parameters)->executeRequest()->get();
@@ -91,6 +136,13 @@ class LibSQL
         return new LibSQLResult($results);
     }
 
+    /**
+     * Execute a query and return the number of affected rows.
+     *
+     * @param string $sql The SQL query.
+     * @param array $parameters The query parameters.
+     * @return int The number of affected rows.
+     */
     public function execute(string $sql, array $parameters = []): int
     {
         $res = $this->http->prepareRequest($sql, $parameters)->executeRequest()->get();
@@ -99,45 +151,101 @@ class LibSQL
         return $result['affected_row_count'];
     }
 
+    /**
+     * Execute a batch of queries.
+     *
+     * @param array $queries The queries to execute.
+     * @throws LibSQLError If there is an error during execution.
+     */
+    public function executeBatch(array $queries)
+    {
+        $trx = $this->transaction();
+        try {
+            foreach ($queries as $query) {
+                $trx->execute($query);
+            }
+            $trx->commit();
+        } catch (Exception $e) {
+            $trx->rollback();
+            throw new LibSQLError($e->getMessage(), "EXECUTE_BATCH_ERROR");
+        }
+    }
+
+    /**
+     * Prepare a statement for execution.
+     *
+     * @param string $sql The SQL statement.
+     * @return LibSQLStatement The prepared statement.
+     */
     public function prepare(string $sql): LibSQLStatement
     {
         return new LibSQLStatement($this, $sql);
     }
 
-    public function transaction(string $mode = 'deffered'): LibSQLTransaction
+    /**
+     * Start a new transaction.
+     *
+     * @param string $mode The transaction mode.
+     * @return LibSQLTransaction The transaction object.
+     * @throws LibSQLError If the transaction mode is invalid.
+     */
+    public function transaction(string $mode = 'deferred'): LibSQLTransaction
     {
         $this->checkTransactionMode($mode);
         return new LibSQLTransaction($this, $mode);
     }
 
+    /**
+     * Get the number of affected rows.
+     *
+     * @return int The number of affected rows.
+     */
     public function changes(): int
     {
         return $this->affected_rows;
     }
 
+    /**
+     * Check if auto commit is enabled.
+     *
+     * @return bool True if auto commit is enabled, false otherwise.
+     */
     public function isAutoCommit(): bool
     {
         return $this->auto_commit;
     }
 
+    /**
+     * Close the database connection.
+     *
+     * @return void
+     */
     public function close(): void
     {
         $this->http->addRequest('close')->executeRequest();
     }
 
-    private function checkTransactionMode(string $mode): LibSQLError|bool
+    /**
+     * Check if the transaction mode is valid.
+     *
+     * @param string $mode The transaction mode.
+     * @return bool True if the mode is valid.
+     * @throws LibSQLError If the mode is invalid.
+     */
+    private function checkTransactionMode(string $mode): bool
     {
-        if(!in_array(strtolower($mode), ['write', 'read', 'deffered'])) {
+        if (!in_array(strtolower($mode), ['write', 'read', 'deferred'])) {
             throw new LibSQLError("Error undefined transaction mode: $mode", "UNDEFINED_TRANSACTION_MODE");
         }
         return true;
     }
 
-    private function setIsAutoCommit(bool $yes): void
-    {
-        $this->auto_commit = $yes;
-    }
-
+    /**
+     * Set the number of affected rows.
+     *
+     * @param array|int $results The query results or the number of affected rows.
+     * @return void
+     */
     private function setChanges(array|int $results): void
     {
         if (is_array($results)) {
