65ad18caeced8c12588c2b8f0508f2c3ecf33544
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-08-03 05:41:43 +0700

    test(database): remove UsingPrepareStatementTest

--- tests/Feature/UsingPrepareStatementTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-it('using prepare statement', function () {
-    createUserTable();
-    $result = $this->db->prepare("INSERT INTO users (:name, :email)")->execute([':name' => 'Test user 1', ':email' => 'testuser1@email.com']) > 0;
-    $result = $this->db->prepare("INSERT INTO users (?, ?)")->execute(['Test user 1', 'testuser1@email.com']) > 0;
-    expect($result)->toBe(true);
-    dropTables("users");
-});
