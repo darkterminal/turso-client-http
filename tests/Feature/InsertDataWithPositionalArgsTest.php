48d138e0f0eb988363f46a60250e5bf9ed95fec5
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-08-03 05:36:14 +0700

    test(database): remove InsertDataWithPositionalArgsTest.php

--- tests/Feature/InsertDataWithPositionalArgsTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-it('inserts data with positional args', function () {
-    createUserTable();
-    $result = $this->db->execute("INSERT INTO users (name, email) VALUES (?, ?)", ['Test User 1', 'testuser1@email.com']) > 0;
-    expect($result)->toBe(true);
-    dropTables("users");
-});
