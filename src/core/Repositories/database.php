064fa04bd9a61c3cad3f030c25fb9b6c89275fa6
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-08-28 11:52:29 +0700

    refactor(database): replace static array with dynamic function for database endpoints

--- src/core/Repositories/database.php
+++ src/core/Repositories/database.php
@@ -1,44 +1,61 @@
 <?php
 
-return [
-    'list'      => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases'
-    ],
-    'create'    => [
-        'method'    => 'POST',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases'
-    ],
-    'retrive'   => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/{databaseName}'
-    ],
-    'usage'   => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/{databaseName}/usage'
-    ],
-    'delete'   => [
-        'method'    => 'DELETE',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/{databaseName}'
-    ],
-    'list_instances'   => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/{databaseName}/instances'
-    ],
-    'get_instance'   => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/{databaseName}/instances/{instanceName}'
-    ],
-    'create_token'   => [
-        'method'    => 'POST',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/{databaseName}/auth/tokens'
-    ],
-    'invalidate_tokens'   => [
-        'method'    => 'POST',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/{databaseName}/auth/rotate'
-    ],
-    'upload_dump'   => [
-        'method'    => 'POST',
-        'url'       => $baseURL . '/organizations/{organizationName}/databases/dumps'
-    ],
-];
+function databaseRepository($action): array
+{
+    $items = [
+        'list' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/databases')
+        ],
+        'create' => [
+            'method' => 'POST',
+            'url' => platform_api_url('/organizations/{organizationName}/databases')
+        ],
+        'retrieve' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}')
+        ],
+        'retrieve_configuration' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/configuration')
+        ],
+        'update_configuration' => [
+            'method' => 'PATCH',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/configuration')
+        ],
+        'usage' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/usage')
+        ],
+        'stats' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/stats')
+        ],
+        'delete' => [
+            'method' => 'DELETE',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}')
+        ],
+        'list_instances' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/instances')
+        ],
+        'retrieve_instance' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/instances/{instanceName}')
+        ],
+        'create_token' => [
+            'method' => 'POST',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/auth/tokens')
+        ],
+        'invalidate_tokens' => [
+            'method' => 'POST',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/{databaseName}/auth/rotate')
+        ],
+        'upload_dump' => [
+            'method' => 'POST',
+            'url' => platform_api_url('/organizations/{organizationName}/databases/dumps')
+        ],
+    ];
+
+    return $items[$action] ?? [];
+}
