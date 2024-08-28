adada2cafe628462a9e78c41dd4ffdd6ec50e32c
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-08-28 11:54:46 +0700

    refactor(organizations): replace static return into dynamic return

--- src/core/Repositories/organizations.php
+++ src/core/Repositories/organizations.php
@@ -1,32 +1,33 @@
 <?php
 
-return [
-    'list' => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations'
-    ],
-    'update' => [
-        'method'    => 'PATCH',
-        'url'       => $baseURL . '/organizations/{organizationName}'
-    ],
-    'members' => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations/{organizationName}/members'
-    ],
-    'add_member' => [
-        'method'    => 'POST',
-        'url'       => $baseURL . '/organizations/{organizationName}/members'
-    ],
-    'remove_member' => [
-        'method'    => 'DELETE',
-        'url'       => $baseURL . '/organizations/{organizationName}/members/{username}'
-    ],
-    'invite_lists' => [
-        'method'    => 'GET',
-        'url'       => $baseURL . '/organizations/{organizationName}/invites'
-    ],
-    'create_invite' => [
-        'method'    => 'POST',
-        'url'       => $baseURL . '/organizations/{organizationName}/invites'
-    ]
-];
+function organizationsRepository($action): array
+{
+    $items = [
+        'list' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations')
+        ],
+        'update' => [
+            'method' => 'PATCH',
+            'url' => platform_api_url('/organizations/{organizationName}')
+        ],
+        'plans' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/plans')
+        ],
+        'subscription' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/subscription')
+        ],
+        'invoices' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/invoices')
+        ],
+        'current_usage' => [
+            'method' => 'GET',
+            'url' => platform_api_url('/organizations/{organizationName}/usage')
+        ],
+    ];
+
+    return $items[$action] ?? [];
+}
