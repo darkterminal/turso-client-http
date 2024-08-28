19c9def46ed597b24162acab3319662a85755e10
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-08-28 11:48:07 +0700

    feat(api-tokens): add results method to streamline response handling

--- src/core/Platform/APITokens.php
+++ src/core/Platform/APITokens.php
@@ -2,6 +2,7 @@
 
 namespace Darkterminal\TursoHttp\core\Platform;
 
+use Darkterminal\TursoHttp\core\LibSQLError;
 use Darkterminal\TursoHttp\core\Response;
 use Darkterminal\TursoHttp\core\Utils;
 
@@ -40,7 +41,13 @@ final class APITokens implements Response
     public function list(): APITokens
     {
         $endpoint = Utils::useAPI('tokens', 'list');
-        $this->response = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+        $tokens = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+
+        if (isset($tokens['error'])) {
+            throw new LibSQLError('Failed to get list of API tokens', 'GET_API_TOKENS_FAILED');
+        }
+        $this->response['list_tokens'] = $tokens['tokens'];
+
         return $this;
     }
 
@@ -54,7 +61,13 @@ final class APITokens implements Response
     public function create(string $tokenName): APITokens
     {
         $endpoint = Utils::useAPI('tokens', 'create');
-        $this->response = Utils::makeRequest($endpoint['method'], \str_replace('{tokenName}', $tokenName, $endpoint['url']), $this->token);
+        $create = Utils::makeRequest($endpoint['method'], \str_replace('{tokenName}', $tokenName, $endpoint['url']), $this->token);
+
+        if (isset($create['error'])) {
+            throw new LibSQLError($create['error'], 'CREATE_API_TOKEN_FAILED');
+        }
+        $this->response['create_token'] = $create;
+
         return $this;
     }
 
@@ -66,7 +79,13 @@ final class APITokens implements Response
     public function validate(): APITokens
     {
         $endpoint = Utils::useAPI('tokens', 'validate');
-        $this->response = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+        $validate = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+
+        if (isset($validate['error'])) {
+            throw new LibSQLError($validate['error'], 'VALIDATE_API_TOKEN_FAILED');
+        }
+        $this->response['token_validate'] = $validate;
+
         return $this;
     }
 
@@ -80,8 +99,30 @@ final class APITokens implements Response
     public function revoke(string $tokenName): APITokens
     {
         $endpoint = Utils::useAPI('tokens', 'revoke');
-        $this->response = Utils::makeRequest($endpoint['method'], \str_replace('{tokenName}', $tokenName, $endpoint['url']), $this->token);
+        $revoke = Utils::makeRequest($endpoint['method'], \str_replace('{tokenName}', $tokenName, $endpoint['url']), $this->token);
+
+        if (isset($revoke['error'])) {
+            throw new LibSQLError($revoke['error'], 'REVOKE_API_TOKEN_FAILED');
+        }
+        $this->response['revoke_token'] = $revoke['token'];
+
         return $this;
+    }
+
+    /**
+     * Returns the result of the previous operation.
+     *
+     * @return array|string The result of the previous operation
+     */
+    private function results(): array|string
+    {
+        return match (true) {
+            isset($this->response['list_tokens']) => $this->response['list_tokens'],
+            isset($this->response['create_token']) => $this->response['create_token'],
+            isset($this->response['token_validate']) => $this->response['token_validate'],
+            isset($this->response['revoke_token']) => $this->response['revoke_token'],
+            default => $this->response,
+        };
     }
 
     /**
@@ -91,16 +132,21 @@ final class APITokens implements Response
      */
     public function get(): array
     {
-        return $this->response;
+        return $this->results();
     }
 
     /**
      * Get the API response as a JSON string or array.
      *
+     * @param bool $pretty Whether to use pretty formatting.
      * @return string|array|null The API response as a JSON string, array, or null if not applicable.
      */
-    public function toJSON(): string|array|null
+    public function toJSON(bool $pretty = false): string|array|null
     {
-        return json_encode($this->response, true);
+        if (!is_array($this->results())) {
+            return $this->results();
+        }
+
+        return $pretty ? json_encode($this->results(), JSON_PRETTY_PRINT) : json_encode($this->results());
     }
 }
