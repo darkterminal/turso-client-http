eb45d4851be702883dd1fe800ece716d4764a3b5
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-08-28 11:51:29 +0700

    feat(organizations): add methods for plans, subscription, invoices, and current usage

--- src/core/Platform/Organizations.php
+++ src/core/Platform/Organizations.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace Darkterminal\TursoHttp\core\Platform;
+
+use Darkterminal\TursoHttp\core\Enums\InvoiceType;
+use Darkterminal\TursoHttp\core\LibSQLError;
 use Darkterminal\TursoHttp\core\Response;
 use Darkterminal\TursoHttp\core\Utils;
 
@@ -38,7 +42,13 @@ final class Organizations implements Response
     public function list(): Organizations
     {
         $endpoint = Utils::useAPI('organizations', 'list');
-        $this->response = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+        $organizations = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+
+        if (isset($organizations['error'])) {
+            throw new LibSQLError('Failed to get list of organizations', 'GET_ORGANIZATIONS_FAILED');
+        }
+        $this->response['list_organizations'] = $organizations;
+        
         return $this;
     }
 
@@ -54,60 +64,99 @@ final class Organizations implements Response
     {
         $endpoint = Utils::useAPI('organizations', 'update');
         $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
-        $this->response = Utils::makeRequest($endpoint['method'], $url, $this->token, ['overages' => $overages]);
+        $update = Utils::makeRequest($endpoint['method'], $url, $this->token, ['overages' => $overages]);
+
+        if (isset($update['error'])) {
+            throw new LibSQLError($update['error'], 'UPDATE_ORGANIZATION_FAILED');
+        }
+        $this->response['update_organization'] = $update;
+
         return $this;
     }
 
     /**
-     * Get a list of members in an organization.
+     * Returns a list of available plans and their quotas.
      *
      * @param string $organizationName The name of the organization.
      *
      * @return Organizations Returns an instance of Organizations for method chaining.
      */
-    public function members(string $organizationName): Organizations
+    public function plans(string $organizationName): Organizations
     {
-        $endpoint = Utils::useAPI('organizations', 'members');
+        $endpoint = Utils::useAPI('organizations', 'plans');
         $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
-        $this->response = Utils::makeRequest($endpoint['method'], $url, $this->token);
+        $plans = Utils::makeRequest($endpoint['method'], $url, $this->token);
+
+        if (isset($plans['error'])) {
+            throw new LibSQLError($plans['error'], 'PLANS_ORGANIZATION_FAILED');
+        }
+        $this->response['plans_organization'] = $plans;
+
         return $this;
     }
 
     /**
-     * Add a member to the organization.
+     * Get the subscription details for an organization.
      *
      * @param string $organizationName The name of the organization.
-     * @param string $role The role of the member.
-     * @param string $username The username of the member.
-     *
-     * @return Organizations|Exception Returns an instance of Organizations for method chaining or throws an Exception on failure.
-     */
-    public function add_member(string $organizationName, string $role, string $username): Organizations|Exception
-    {
-        $endpoint = Utils::useAPI('organizations', 'add_member');
-        $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
-        Utils::validateMemberRole($role);
-        $this->response = Utils::makeRequest($endpoint['method'], $url, $this->token, [
-            'role' => $role,
-            'username' => $username
-        ]);
-        return $this;
-    }
-
-    /**
-     * Remove a member from the organization.
-     *
-     * @param string $organizationName The name of the organization.
-     * @param string $username The username of the member to be removed.
      *
      * @return Organizations Returns an instance of Organizations for method chaining.
      */
-    public function remove_member(string $organizationName, string $username): Organizations
+    public function subscription(string $organizationName): Organizations
     {
-        $endpoint = Utils::useAPI('organizations', 'remove_member');
+        $endpoint = Utils::useAPI('organizations', 'subscription');
         $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
-        $url = \str_replace('{username}', $username, $endpoint['url']);
-        $this->response = Utils::makeRequest($endpoint['method'], $url, $this->token);
+        $subscription = Utils::makeRequest($endpoint['method'], $url, $this->token);
+
+        if (isset($subscription['error'])) {
+            throw new LibSQLError($subscription['error'], 'SUBSCRIPTION_ORGANIZATION_FAILED');
+        }
+        $this->response['subscription_organization'] = $subscription['subscription'];
+
+        return $this;
+    }
+
+    /**
+     * Get a list of invoices for an organization.
+     *
+     * @param string $organizationName The name of the organization.
+     * @param InvoiceType $invoiceType The type of invoices to retrieve. Defaults to InvoiceType::ALL.
+     *
+     * @return Organizations Returns an instance of Organizations for method chaining.
+     */
+    public function invoices(string $organizationName, InvoiceType $invoiceType = InvoiceType::ALL): Organizations
+    {
+        $endpoint = Utils::useAPI('organizations', 'invoices');
+        $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
+        $url = "?type={$invoiceType->value}";
+        $invoices = Utils::makeRequest($endpoint['method'], $url, $this->token);
+
+        if (isset($invoices['error'])) {
+            throw new LibSQLError($invoices['error'], 'INVOICES_ORGANIZATION_FAILED');
+        }
+        $this->response['invoices_organization'] = empty($invoices) ? "There is no invoices for {$organizationName}" : $invoices;
+
+        return $this;
+    }
+
+    /**
+     * Get the current usage for an organization.
+     *
+     * @param string $organizationName The name of the organization.
+     *
+     * @return Organizations Returns an instance of Organizations for method chaining.
+     */
+    public function currentUsage(string $organizationName): Organizations
+    {
+        $endpoint = Utils::useAPI('organizations', 'current_usage');
+        $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
+        $currentUsage = Utils::makeRequest($endpoint['method'], $url, $this->token);
+
+        if (isset($currentUsage['error'])) {
+            throw new LibSQLError($currentUsage['error'], 'CURRENTUSAGE_ORGANIZATION_FAILED');
+        }
+        $this->response['current_usage_organization'] = $currentUsage['organization'];
+
         return $this;
     }
 
@@ -118,7 +167,7 @@ final class Organizations implements Response
      *
      * @return Organizations Returns an instance of Organizations for method chaining.
      */
-    public function invite_lists(string $organizationName): Organizations
+    public function inviteLists(string $organizationName): Organizations
     {
         $endpoint = Utils::useAPI('organizations', 'invite_lists');
         $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
@@ -135,7 +184,7 @@ final class Organizations implements Response
      *
      * @return Organizations Returns an instance of Organizations for method chaining.
      */
-    public function create_invite(string $organizationName, string $role, string $username): Organizations
+    public function createInvite(string $organizationName, string $role, string $username): Organizations
     {
         $endpoint = Utils::useAPI('organizations', 'create_invite');
         $url = \str_replace('{organizationName}', $organizationName, $endpoint['url']);
@@ -148,22 +197,44 @@ final class Organizations implements Response
     }
 
     /**
+     * Returns the result of the previous operation.
+     *
+     * @return array|string The result of the previous operation
+     */
+    private function results(): array|string
+    {
+        return match (true) {
+            isset($this->response['list_organizations']) => $this->response['list_organizations'],
+            isset($this->response['update_organization']) => $this->response['update_organization'],
+            isset($this->response['subscription_organization']) => $this->response['subscription_organization'],
+            isset($this->response['invoices_organization']) => $this->response['invoices_organization'],
+            isset($this->response['current_usage_organization']) => $this->response['current_usage_organization'],
+            default => $this->response,
+        };
+    }
+
+    /**
      * Get the API response as an array.
      *
      * @return array The API response as an array.
      */
     public function get(): array
     {
-        return $this->response;
+        return $this->results();
     }
 
     /**
      * Get the API response as a JSON string or array.
      *
+     * @param bool $pretty Whether to use pretty formatting.
      * @return string|array|null The API response as a JSON string, array, or null if not applicable.
      */
-    public function toJSON(): string|array|null
+    public function toJSON(bool $pretty = false): string|array|null
     {
-        return json_encode($this->response, true);
+        if (!is_array($this->results())) {
+            return $this->results();
+        }
+
+        return $pretty ? json_encode($this->results(), JSON_PRETTY_PRINT) : json_encode($this->results());
     }
 }
