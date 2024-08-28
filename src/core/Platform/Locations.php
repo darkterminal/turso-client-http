54a7c13c581c536b1038077e84b361c3f487636b
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-08-28 11:50:51 +0700

    refactor(locations): update method names to camelCase

--- src/core/Platform/Locations.php
+++ src/core/Platform/Locations.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Darkterminal\TursoHttp\core\Platform;
+
+use Darkterminal\TursoHttp\core\LibSQLError;
 use Darkterminal\TursoHttp\core\Response;
 use Darkterminal\TursoHttp\core\Utils;
 
@@ -35,10 +38,16 @@ final class Locations implements Response
      *
      * @return Locations Returns an instance of Locations for method chaining.
      */
-    public function get_locations(): Locations
+    public function getLocations(): Locations
     {
         $endpoint = Utils::useAPI('locations', 'list');
-        $this->response = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+        $locations = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+
+        if (!isset($locations['locations'])) {
+            throw new LibSQLError('Failed to get list of locations', 'GET_LOCATIONS_FAILED');
+        }
+        $this->response['list_locations'] = $locations['locations'];
+
         return $this;
     }
 
@@ -47,11 +56,31 @@ final class Locations implements Response
      *
      * @return Locations Returns an instance of Locations for method chaining.
      */
-    public function closest_region(): Locations
+    public function closestRegion(): Locations
     {
-        $endpoint = Utils::useAPI('location', 'closest_region');
-        $this->response = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+        $endpoint = Utils::useAPI('locations', 'closest_region');
+        $closest = Utils::makeRequest($endpoint['method'], $endpoint['url'], $this->token);
+
+        if (isset($closest['error'])) {
+            throw new LibSQLError('Failed to get closest region', 'GET_CLOSEST_REGION_FAILED');
+        }
+        $this->response['closest_region'] = $closest;
+
         return $this;
+    }
+
+    /**
+     * Returns the result of the previous operation.
+     *
+     * @return array|string The result of the previous operation
+     */
+    private function results(): array|string
+    {
+        return match (true) {
+            isset($this->response['list_locations']) => $this->response['list_locations'],
+            isset($this->response['closest_region']) => $this->response['closest_region'],
+            default => $this->response,
+        };
     }
 
     /**
@@ -61,16 +90,21 @@ final class Locations implements Response
      */
     public function get(): array
     {
-        return $this->response;
+        return $this->results();
     }
 
     /**
      * Get the API response as a JSON string or array.
      *
+     * @param bool $pretty Whether to use pretty formatting.
      * @return string|array|null The API response as a JSON string, array, or null if not applicable.
      */
-    public function toJSON(): string|array|null
+    public function toJSON(bool $pretty = false): string|array|null
     {
-        return json_encode($this->response, true);
+        if (!is_array($this->results())) {
+            return $this->results();
+        }
+
+        return $pretty ? json_encode($this->results(), JSON_PRETTY_PRINT) : json_encode($this->results());
     }
 }
